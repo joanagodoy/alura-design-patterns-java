c256d8c25f5b6ca3443b98bd6dd99907cb1a7561
joanagodoy <joanapereiragodoy@gmail.com>
2022-02-08 09:47:05 -0300

    Classe que retorna calculo de imposto

---
+++ src/main/java/br/alura/loja/imposto/CalculadoraImpostos.java
@@ -0,0 +1,12 @@
+package br.alura.loja.imposto;
+
+import br.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class CalculadoraImpostos {
+
+    public BigDecimal calcular(Orcamento orcamento, Imposto imposto){
+        return imposto.calcular(orcamento);
+    }
+}
